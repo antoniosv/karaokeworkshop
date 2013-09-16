e77015b1c065f929acdba2a956d39e558b8ceef2
Jesus Antonio <sephiran44@hotmail.com>
2013-09-15 22:44:09 -0500

    Demo simple de envio de stream de server a cliente

---
+++ smalldemos/server2client/server.js
@@ -0,0 +1,11 @@
+var BinaryServer = require('binaryjs').BinaryServer;
+var fs = require('fs');
+
+var server = BinaryServer({port: 9000});
+
+// callback function
+server.on('connection', function(client) {
+   // var file = fs.createReadStream(__dirname + '/classic.ogg');
+    var file = fs.createReadStream(__dirname + '/flower.png');
+    client.send(file);
+});
